a963cf60df81a0a14327b4435c368e27c6f84e1c
Caio Ferreira <caiosantana757@gmail.com>
2025-04-24 15:28:04 -0300

    Atualização da peninsula iberica

--- PrimeirosProgramas/Parte1/cronometro.py
+++ PrimeirosProgramas/Parte1/cronometro.py
@@ -3,13 +3,13 @@ from time import sleep
 
 # Definindo a função principal
 def main(page: ft.Page):
-    page.title = "Contador" # Colocando o texto do app
+    page.title = "App de cronometrô" # Colocando o texto do app
 
     text = ft.Text() # Variavel de input
     page.add(text) # Adicionar a página
 
     for i in range(1,11):
-        text.value = "Contador " + str(i)
+        text.value = "Cronometrô: " + str(i)
         page.update()
         sleep(1) # Vai fazer o programa parar por 1 segundo
 
